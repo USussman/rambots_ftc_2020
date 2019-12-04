0be2e4ee609a30cf781ee2be97a6dae546ba72f6
= <=>
2019-12-04 18:56:37 -0500

    Create movement classes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Claw {
+
+    private Servo hand;
+    private Servo wrist
+    private Servo shoulderRotate
+    private DcMotor shoulderElevate;
+
+
+    public Claw(DcMotor lm, DcMotor rm){
+
+    }
+    public void move(float forwardSpeed, float upSpeed, float rotateDegrees)
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOp.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created 11/13/19 by Rambots Team
+ */
+
+@TeleOp
+
+public class TestOp extends LinearOpMode {
+    private Gyroscope imu;
+    private DcMotor motorTest;
+    private DigitalChannel digitalTouch;
+    private DistanceSensor sensorColorRange;
+    private Servo servoTest;
+
+    @Override
+    public void runOpMode() {
+        imu = hardwareMap.get(Gyroscope.class, imu);
+
+    }
+}
