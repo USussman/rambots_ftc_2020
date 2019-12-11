ac23da725153266c8d9af39667a0920b30b171f6
= <=>
2019-12-11 18:10:02 -0500

    started claw code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.java
@@ -25,14 +25,16 @@ public class Wheels {
         rightMotor.setPower(speed/(22.5 * Math.PI)); //divide by max speed in order to get number in range [-1.0, 1.0]
     }
 
-    public void turn(double speed, double angle, double turnRadius){
-        float innerSpeed = speed/(1+width/turnRadius);
+    public void turn(double outerSpeed, double angle, double turnRadius){
+        double innerSpeed = outerSpeed/(1+width/turnRadius);
         ((angle < 0) ? leftMotor : rightMotor).setPower(innerSpeed/(22.5 * Math.PI)); //divide by max speed in order to get number in range [-1.0, 1.0]
-        ((angle > 0) ? leftMotor : rightMotor).setPower(this.speed/(22.5 * Math.PI)); //divide by max speed in order to get number in range [-1.0, 1.0]
+        ((angle > 0) ? leftMotor : rightMotor).setPower(outerSpeed/(22.5 * Math.PI)); //divide by max speed in order to get number in range [-1.0, 1.0]
         ElapsedTime time = new ElapsedTime();
         time.reset();
-        while(time.time() < ());
-
+        //wait to complete turn
+        double radians = (angle*Math.PI)/180
+        while(time.time() < (speed/(radians*(turnRadius + width))));
+        //go back to driving
         leftMotor.setPower(this.speed/(22.5 * Math.PI)); //divide by max speed in order to get number in range [-1.0, 1.0]
         rightMotor.setPower(this.speed/(22.5 * Math.PI)); //divide by max speed in order to get number in range [-1.0, 1.0]
     }
