c533b6560be6eb8fffa09005e374cda78eec06ca
= <=>
2019-12-20 14:00:11 -0500

    Finished Claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -5,27 +5,39 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw {
 
-    private Servo hand;
-    private Servo wrist
-    private Servo shoulderRotate
+    private Servo wrist;
+    private Servo elbow;
+    private Servo shoulderRotate;
     private DcMotor shoulderElevate;
+    private int armA = 420;//mm
+    private int armB = 225;//mm
 
-
-    public Claw(Servo hand, Servo wrist, Servo shoulderRotate, DcMotor shoulderElevate){
-        this.hand = hand;
+    public Claw(Servo wrist, Servo elbow, Servo shoulderRotate, DcMotor shoulderElevate){
         this.wrist = wrist;
+        this.elbow = elbow;
         this.shoulderRotate = shoulderRotate;
         this.shoulderElevate = shoulderElevate;
         shoulderElevate.setDirection(DcMotor.Direction.FORWARD); //might need to reverse
+        wrist.setPosition(0);
+        elbow.setPosition(0);
+        shoulderRotate.setPosition(0);
+        shoulderElevate.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     /**
      *
-     * @param forwardSpeed
-     * @param upSpeed
-     * @param rotateSpeed between 0 and 1
+     * @param moveX in mm
+     * @param moveY in mm
+     * @param rotateDegrees between -1 and 1
      */
-    public void move(float forwardSpeed, float upSpeed, float rotateSpeed){
-        shoulderElevate.setPower(rotateSpeed)
+    public void move(int moveX, int moveY, double rotateDegrees){
+        int r = armA * armA + armB * armB - 2 * armA * armB * (int) Math.cos(elbow.getPosition());
+        int x = (int) Math.cos(shoulderElevate.getTargetPosition()) * r;
+        int y = (int) Math.sin(shoulderElevate.getTargetPosition()) * r;
+        x+=moveX;
+        y+=moveY;
+        shoulderElevate.setTargetPosition((int)Math.atan(y/x));
+        elbow.setPosition(Math.acos(x*x+y*y-armA*armA-armB*armB+2*armA*armB));
+        shoulderRotate.setPosition(rotateDegrees);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class Wheels {
     //two hex motors 150rpm 90pi mm circumference
     private double speed; //positive is forward, negative is backward. measured in cm/sec
-    private static final int width= 36; //in cm
+    private static final int width= 360; //in mm
 
     private DcMotor leftMotor;
     private DcMotor rightMotor;
@@ -37,5 +37,17 @@ public class Wheels {
         //go back to driving
         leftMotor.setPower(this.speed/(22.5 * Math.PI)); //divide by max speed in order to get number in range [-1.0, 1.0]
         rightMotor.setPower(this.speed/(22.5 * Math.PI)); //divide by max speed in order to get number in range [-1.0, 1.0]
+    }
+
+    /**
+     *
+     * @param outerSpeed cm/s
+     * @param turnDirection true is right. false is left
+     * @param turnRadius in mm
+     */
+    public void turn(double outerSpeed, boolean turnDirection, double turnRadius){
+        double innerSpeed = outerSpeed/(1+width/turnRadius);
+        (!turnDirection ? leftMotor : rightMotor).setPower(innerSpeed/(22.5 * Math.PI)); //divide by max speed in order to get number in range [-1.0, 1.0]
+        (turnDirection ? leftMotor : rightMotor).setPower(outerSpeed/(22.5 * Math.PI)); //divide by max speed in order to get number in range [-1.0, 1.0]
     }
 }
