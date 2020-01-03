7ffda18893bf705638dd96a3c845d8b633ce59c0
AS <ariel.schanke@gmail.com>
2020-01-02 22:19:15 -0500

    Finished accelerometer? Needs testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LSM303.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LSM303.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice;
 import com.qualcomm.robotcore.hardware.configuration.I2cSensor;
-import com.qualcomm.robotcore.util.TypeConversion;
 
 @SuppressWarnings({"unused"})
 
@@ -12,41 +11,83 @@ import com.qualcomm.robotcore.util.TypeConversion;
 @I2cSensor(name = "LSM303 Accelerometer", description = "Accelerometer from Adafruit", xmlTag = "LSM303")
 public class LSM303 extends I2cDeviceSynchDevice<I2cDeviceSynch> {
 
-    public final static I2cAddr ADDRESS_I2C_DEFAULT = new I2cAddr(0b11001);
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Raw Register Reads
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public byte getCTRL_REG1_A()
+    {
+        // Method to test if everything is working. Should return 0b00000111 (7)
+        return readByte(Register.ACCEL_CTRL_REG1_A);
+    }
+
+    public int[] getAccelerometer() {
+        // Get the raw data
+        byte xL = readByte(Register.ACCEL_OUT_X_L_A);
+        byte xH = readByte(Register.ACCEL_OUT_X_H_A);
+        byte yL = readByte(Register.ACCEL_OUT_Y_L_A);
+        byte yH = readByte(Register.ACCEL_OUT_Y_H_A);
+        byte zL = readByte(Register.ACCEL_OUT_Z_L_A);
+        byte zH = readByte(Register.ACCEL_OUT_Z_H_A);
+
+        // Shift to create an int
+        int x = ((xH << 8) | xL);
+        int y = ((yH << 8) | yL);
+        int z = ((zH << 8) | zL);
+        return new int[]{x,y,z};
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Read and Write Methods
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    protected void writeByte(final Register reg, byte value)
+    {
+        deviceClient.write8(reg.bVal,value);
+    }
+
+    protected byte readByte(Register reg)
+    {
+        return deviceClient.read8(reg.bVal);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Registers and Config Settings
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     public enum Register {
+        // Currently only for accelerometer
         FIRST (0),
-        CTRL_REG1_A (0x20),   // 00000111   rw
-        CTRL_REG2_A (0x21),   // 00000000   rw
-        CTRL_REG3_A (0x22),   // 00000000   rw
-        CTRL_REG4_A (0x23),   // 00000000   rw
-        CTRL_REG5_A (0x24),   // 00000000   rw
-        CTRL_REG6_A (0x25),   // 00000000   rw
-        REFERENCE_A (0x26),   // 00000000   r
-        STATUS_REG_A (0x27),   // 00000000   r
-        OUT_X_L_A (0x28),
-        OUT_X_H_A (0x29),
-        OUT_Y_L_A (0x2A),
-        OUT_Y_H_A (0x2B),
-        OUT_Z_L_A (0x2C),
-        OUT_Z_H_A (0x2D),
-        FIFO_CTRL_REG_A (0x2E),
-        FIFO_SRC_REG_A (0x2F),
-        INT1_CFG_A (0x30),
-        INT1_SOURCE_A (0x31),
-        INT1_THS_A (0x32),
-        INT1_DURATION_A (0x33),
-        INT2_CFG_A (0x34),
-        INT2_SOURCE_A (0x35),
-        INT2_THS_A (0x36),
-        INT2_DURATION_A (0x37),
-        CLICK_CFG_A(0x38),
-        CLICK_SRC_A(0x39),
-        CLICK_THS_A(0x3A),
-        TIME_LIMIT_A (0x3B),
-        TIME_LATENCY_A (0x3C),
-        TIME_WINDOW_A (0x3D),
-        LAST(TIME_WINDOW_A.bVal);
+        ACCEL_CTRL_REG1_A(0x20),   // 00000111   rw
+        ACCEL_CTRL_REG2_A(0x21),   // 00000000   rw
+        ACCEL_CTRL_REG3_A(0x22),   // 00000000   rw
+        ACCEL_CTRL_REG4_A(0x23),   // 00000000   rw
+        ACCEL_CTRL_REG5_A(0x24),   // 00000000   rw
+        ACCEL_CTRL_REG6_A(0x25),   // 00000000   rw
+        ACCEL_REFERENCE_A(0x26),   // 00000000   r
+        ACCEL_STATUS_REG_A (0x27),   // 00000000   r
+        ACCEL_OUT_X_L_A (0x28),
+        ACCEL_OUT_X_H_A (0x29),
+        ACCEL_OUT_Y_L_A (0x2A),
+        ACCEL_OUT_Y_H_A (0x2B),
+        ACCEL_OUT_Z_L_A (0x2C),
+        ACCEL_OUT_Z_H_A (0x2D),
+        ACCEL_FIFO_CTRL_REG_A (0x2E),
+        ACCEL_FIFO_SRC_REG_A (0x2F),
+        ACCEL_INT1_CFG_A (0x30),
+        ACCEL_INT1_SOURCE_A (0x31),
+        ACCEL_INT1_THS_A (0x32),
+        ACCEL_INT1_DURATION_A (0x33),
+        ACCEL_INT2_CFG_A (0x34),
+        ACCEL_INT2_SOURCE_A (0x35),
+        ACCEL_INT2_THS_A (0x36),
+        ACCEL_INT2_DURATION_A (0x37),
+        ACCEL_CLICK_CFG_A (0x38),
+        ACCEL_CLICK_SRC_A (0x39),
+        ACCEL_CLICK_THS_A (0x3A),
+        ACCEL_TIME_LIMIT_A (0x3B),
+        ACCEL_TIME_LATENCY_A (0x3C),
+        ACCEL_TIME_WINDOW_A (0x3D),
+        LAST(ACCEL_TIME_WINDOW_A.bVal);
 
         public int bVal;
 
@@ -54,6 +95,12 @@ public class LSM303 extends I2cDeviceSynchDevice<I2cDeviceSynch> {
             this.bVal = bVal;
         }
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Construction and Initialization
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public final static I2cAddr ADDRESS_I2C_DEFAULT = I2cAddr.create7bit(0b0011001);
 
     public LSM303(I2cDeviceSynch deviceClient)
     {
@@ -64,16 +111,6 @@ public class LSM303 extends I2cDeviceSynchDevice<I2cDeviceSynch> {
 
         super.registerArmingStateCallback(false);
         this.deviceClient.engage();
-    }
-
-    protected void writeByte(final Register reg, byte value)
-    {
-        deviceClient.write8(reg.bVal,value);
-    }
-
-    protected byte readByte(Register reg)
-    {
-        return deviceClient.read8(reg.bVal);
     }
 
     protected void setOptimalReadWindow() {
@@ -87,12 +124,6 @@ public class LSM303 extends I2cDeviceSynchDevice<I2cDeviceSynch> {
     }
 
     @Override
-    public Manufacturer getManufacturer()
-    {
-        return Manufacturer.Adafruit;
-    }
-
-    @Override
     protected synchronized boolean doInitialize()
     {
         return true;
@@ -102,5 +133,11 @@ public class LSM303 extends I2cDeviceSynchDevice<I2cDeviceSynch> {
     public String getDeviceName()
     {
         return "Adafruit LSM303 Accelerometer";
+    }
+
+    @Override
+    public Manufacturer getManufacturer()
+    {
+        return Manufacturer.Adafruit;
     }
 }
