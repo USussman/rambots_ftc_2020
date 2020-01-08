1094bcc857b02306243c739ce842d4b73f96c9b8
Uriel Sussman <urielsussman@gmail.com>
2020-01-08 08:54:30 -0500

    Change implementation to extension.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class Wheels implements Runnable{
+public class Wheels extends Thread{
     //two hex motors 150rpm 90pi mm circumference
     private double speed; //positive is forward, negative is backward. measured in cm/sec
     private static final int width = 360; //in mm
