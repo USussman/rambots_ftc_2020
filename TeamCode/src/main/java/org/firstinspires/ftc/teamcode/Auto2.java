484508d10345e8a5184ecd50925b0d35ae515e77
AS <ariel.schanke@gmail.com>
2020-01-19 18:34:17 -0500

    Competition day changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto2.java
@@ -7,9 +7,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name="AutoOpMode", group="AutoOpModes")
+@Autonomous(name="AutoOpMode2", group="AutoOpModes")
 
-public class Auto extends LinearOpMode {
+public class Auto2 extends LinearOpMode {
     private Servo hand;
     private CRServo wristServo;
     private CRServo elbowServo;
@@ -47,31 +47,36 @@ public class Auto extends LinearOpMode {
         claw.wrist.setPower(-1);//1*/
         waitForStart();
         runtime.reset();
-        double speed =  22.5 * Math.PI;
-        while (opModeIsActive()) {
+        double speed =  60;
+       // while (opModeIsActive()) {
             /*claw.claw.shoulderElevate.setPower(0);
             claw.elbow.setPower(0;//2
             claw.shoulderRotate.setPower(0);//0
             claw.wrist.setPower(0);//1*/
 
             //moveFoward(5, speed);
-            //turn(speed ,false,1);
-            moveFoward(60, speed);
+            runtime.reset();
+            while(runtime.seconds()<20){}
+                //turn(speed ,false,1);
+            moveFoward(0.75, speed);
+            turn(0.7 ,true);
+            moveFoward(0.9, speed);
 
-        }
+        //}
     }
 
     /**
      *
-     * @param distance to move in cm
+     * @param distance to move in 1 = 240
      * @param speed of the robot in cm/s
      */
     public void moveFoward(double distance, double speed){
         runtime.reset();
         wheels.rightMotor.setPower(1);
         wheels.leftMotor.setPower(1);
-        while((runtime.time()-runtime.startTime())*speed<distance) {
-
+        while(runtime.seconds()<distance) {
+            telemetry.addData("Time", runtime.time()-runtime.startTime());
+            telemetry.update();
         }
         wheels.rightMotor.setPower(0);
         wheels.leftMotor.setPower(0);
@@ -92,14 +97,14 @@ public class Auto extends LinearOpMode {
      * //@param distance to move in cm
      * @param speed of the robot in cm/s
      * @param turnDirection true if right
-     * @param angleAdjustFactor 1 for 90 degrees, 2 for 45, etc
+     * @param //angleAdjustFactor 1 for 90 degrees, 2 for 45, etc
      */
-    public void turn(double speed, boolean turnDirection, double angleAdjustFactor){
-        double distance = 36/angleAdjustFactor;
+    public void turn(double speed, boolean turnDirection){//, double angleAdjustFactor){
+        double distance = 36;//angleAdjustFactor;
         runtime.reset();
         wheels.rightMotor.setPower(turnDirection?-1:1);
         wheels.leftMotor.setPower(turnDirection?1:-1);
-        while((runtime.time()-runtime.startTime())*speed<distance) {
+        while((runtime.seconds())<speed) {
 
         }
         wheels.rightMotor.setPower(0);
